c06089328fd898c749214b178c22a2cd49df683d
Regan <mwangifelix602@gmail.com>
2025-04-06 12:53:55 +0300

    'frontend'

--- Backend/routes/expense.js
+++ Backend/routes/expense.js
@@ -4,28 +4,29 @@ const Expense = require("../Models/Expense");
 const authenticateToken = require("../middleware/authMiddleware"); // Import your authentication middleware
 
 // Add an Expense (requires authentication)
-router.post("/", authenticateToken, async (req, res) => {
+router.post("/", async (req, res) => {
     try {
         const newExpense = new Expense({ ...req.body, userId: req.user.userId }); // Assuming req.user.userId is set in the token
         const savedExpense = await newExpense.save();
         res.status(201).json(savedExpense);
     } catch (error) {
-        res.status(500).json({ message: "Server Error", error });
+        res.status(500).json( error);
     }
 });
 
-// Get All Expenses (requires authentication)
-router.get("/", authenticateToken, async (req, res) => {
+// Get All Expenses 
+router.get("/",  async (req, res) => {
     try {
         const expenses = await Expense.find({ userId: req.user.userId }).sort({ createdAt: -1 });
         res.status(200).json(expenses);
     } catch (error) {
-        res.status(500).json({ message: "Server Error", error });
+        res.status(500).json( error );
     }
 });
 
-// Update an Expense (requires authentication)
-router.put("/:id", authenticateToken, async (req, res) => {
+
+// Update Expense
+router.put("/:id", async (req, res) => {
     try {
         const updatedExpense = await Expense.findByIdAndUpdate(
             req.params.id,
@@ -34,17 +35,17 @@ router.put("/:id", authenticateToken, async (req, res) => {
         );
         res.status(200).json(updatedExpense);
     } catch (error) {
-        res.status(500).json({ message: "Server Error", error });
+        res.status(500).json(error);
     }
 });
 
 // Delete an Expense (requires authentication)
-router.delete("/:id", authenticateToken, async (req, res) => {
+router.delete("/:id", async (req, res) => {
     try {
         await Expense.findByIdAndDelete(req.params.id);
         res.status(200).json({ message: "Expense deleted successfully" });
     } catch (error) {
-        res.status(500).json({ message: "Server Error", error });
+        res.status(500).json(error);
     }
 });
 
